3c69ec2bf69276cb962b2dfc9daa1199c21aa657
vedran maras <vedran.maras@gmail.com>
2018-01-30 22:00:33 +0100

    exercise 1

---
+++ dom-exercises/scripts/index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var str = "Lorem Ipsum dolor samet";
+    var result = str.fontcolor("red");
+    document.getElementById("para").style.fontSize = "xx-large";
+    document.getElementById("para").innerHTML = result;
+}
